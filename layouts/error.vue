54a3436d0b1b310058910f1135ef6c7042cca720
Alan Jimenez Cordoba <alaanjimenez03@gmail.com>
2020-02-20 16:11:33 -0300

    Nout app ready again

---
+++ layouts/error.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+    <h1>{{error.statusCode}}</h1>
+    <div v-if="error.statusCode === 404" class="msg">
+      <h2>Página no encontrada</h2>
+      <p>Lo que estabas buscando no se encuentra aquí.</p>
+    </div>
+    <div v-else-if="error.statusCode >= 500" class="msg">
+      <h2>El servidor no responde</h2>
+      <p>Ha ocurrido un error en el servidor.</p>
+    </div>
+    <simple-button
+      text="Volver a Inicio"
+      to="/"
+      color="tertiary">
+    </simple-button>
+  </div>
+</template>
+
+<script>
+import button from "@/components/buttons/button"
+
+export default {
+  components: {
+    'simple-button': button
+  },
+  props: ['error']
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  width: 100%;
+  padding: 0 2rem;
+  margin: 6rem auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  & h1 {
+    font-size: 6rem;
+    font-family: $lato;
+    font-weight: 400;
+  }
+  & .msg {
+    margin-bottom: 2rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    & h2 {
+      margin-bottom: 1rem;
+      font-size: 1.5rem;
+    }
+    & p {
+      width: 12rem;
+    }
+  }
+}
+</style>
---
+++ pages/signin.vue
@@ -0,0 +1,36 @@
+<template>
+	<signin-form
+		context="signin"
+		title="Completá con tus datos"
+		storeModule="signin"
+		btnText="Registrarse">
+		<nuxt-link to="/login">Volver a Login</nuxt-link>
+	</signin-form>
+</template>
+
+<script>
+import signinForm from "@/components/form";
+
+export default {
+	data() {
+		return {
+
+		};
+	},
+	layout: 'session',
+	components: {
+		"signin-form": signinForm
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+	min-height: 100vh;
+	width: 100%;
+}
+
+a {
+	font-weight: 600;
+}
+</style>
