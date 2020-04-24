07ef3f1f95dfb7190458f79c0a2f39fc35eeae8c
Alan Jimenez Cordoba <alaanjimenez03@gmail.com>
2020-04-24 20:55:40 -0300

    Fixed auth bug

--- middleware/authenticated.ts
+++ middleware/authenticated.ts
@@ -2,7 +2,10 @@ import { Middleware } from '@nuxt/types'
 
 const authenticated: Middleware = ({ store, redirect }) => {
   const logged: boolean = store.state.user.logged
-  if (!logged) {
+  const _id: string | null = localStorage.getItem('id')
+  const token: string | null = localStorage.getItem('token')
+
+  if (!logged || !_id || !token) {
     return redirect('/login')
   }
 }
